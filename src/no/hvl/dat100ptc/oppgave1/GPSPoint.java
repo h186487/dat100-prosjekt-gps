fbbc2a4170d1e79b69f443f94a911e0a7268862d
h186487 <nadia.lambrecht@gmail.com>
2024-10-03 17:06:45 +0200

    flyttet til main og get-set metoder

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -21,65 +21,54 @@ public class GPSPoint {
 //		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
 		
 	}
-	
-	public static void main (String[] args) {
-		GPSPoint point = new GPSPoint(10, 5.5, 4.5, 12);
-		
-		System.out.println("Tid: " + point.time + " sekunder");
-		System.out.println("Høydegrad: " + point.latitude);
-		System.out.println("Lengdegrad: " + point.longitude);
-		System.out.println("Høyde: " + point.elevation + " meter");
-
-		
-	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return time;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
+		this.time = time;	
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return latitude;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 		
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.latitude = latitude;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return longitude;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.longitude = longitude;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return elevation;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.elevation = elevation;
+//		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 	
--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		GPSPoint point = new GPSPoint(0, 0, 0, 0);
+		
+        point.setTime(20);
+        point.setLatitude(60.385390);
+        point.setLongitude(5.332200);
+        point.setElevation(75.0);
+		
+		System.out.println("Tid: " + point.getTime() + " sekunder");
+		System.out.println("Høydegrad: " + point.getLatitude());
+		System.out.println("Lengdegrad: " + point.getLongitude());
+		System.out.println("Høyde: " + point.getElevation() + " meter");
+		
 		
 	}
 
