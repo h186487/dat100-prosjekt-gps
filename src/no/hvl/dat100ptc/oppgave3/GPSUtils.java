70e72898c6d5a0dd5b30a2c52214f89bd2c1b33b
h186487 <nadia.lambrecht@gmail.com>
2024-10-04 16:44:46 +0200

    add kommentarer og oppgave e,f

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -39,8 +39,9 @@ public class GPSUtils {
 
 	public static double[] getLatitudes(GPSPoint[] gpspoints) {
 		
+//		initialiser array variabel som lagrer breddegrad verdier for gps-punkter
 		double[] latitudes = new double[gpspoints.length];
-		
+//		gå gjennom gps-punkt og hent breddegrad, lagre breddegrad verdier i latitudes arrayen
 		for (int i = 0; i < gpspoints.length; i++) {
 			latitudes[i] = gpspoints[i].getLatitude();
 		}
@@ -49,9 +50,9 @@ public class GPSUtils {
 	}
 
 	public static double[] getLongitudes(GPSPoint[] gpspoints) {
-
+//		initialiser array variabel som lagrer lengdegrad verdier for gps-punkter
 		double[] longitudes = new double[gpspoints.length];
-		
+//		gå gjennom gps-punkt og hent ut lengdegrad, lagre lengdegrad verdier i longitudes arrayen
 		for (int i = 0; i < gpspoints.length; i++) {
 			longitudes[i] = gpspoints[i].getLongitude();
 		}
@@ -65,7 +66,7 @@ public class GPSUtils {
 
 		double d;
 		double latitude1, longitude1, latitude2, longitude2;
-
+//		definer variabler og omregn til radianer
 		latitude1 = Math.toRadians(gpspoint1.getLatitude());
 		latitude2 = Math.toRadians(gpspoint2.getLatitude());
 		longitude1 = Math.toRadians(gpspoint1.getLongitude());
@@ -73,7 +74,7 @@ public class GPSUtils {
 
 		double deltaphi = latitude2 - latitude1;
 		double deltadelta = longitude2 - longitude1;
-		
+//		definer og hent variabler fra compute_a og b metoder
 		double a = compute_a(latitude1, latitude2, deltaphi, deltadelta);
 		double c = compute_c(a);
 		d = R * c;
@@ -82,7 +83,7 @@ public class GPSUtils {
 	}
 	
 	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
-	
+//		definer variabler f og g som inneholder del av mattestykket som skal opphøyes i 2
 		double f = (Math.sin(deltaphi / 2));
 		double g = (Math.sin(deltadelta / 2));
 		
@@ -101,24 +102,35 @@ public class GPSUtils {
 	
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
 
-		int secs;
 		double speed;
+//		definer variabler som henter tid fra gpspoint
+		int time1 = gpspoint1.getTime();
+		int time2 = gpspoint2.getTime();
+		int secs = time2 - time1;
+
+		double distance = distance(gpspoint1, gpspoint2);
+
+		speed = distance / secs;
+		return speed;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
-		// TODO
 
 	}
 
 	public static String formatTime(int secs) {
 
-		String timestr;
+
 		String TIMESEP = ":";
 
-		throw new UnsupportedOperationException(TODO.method());
+		int timer = secs / 3600; //1 time = 3600 sekunder
+		int minutter = (secs % 3600) / 60; //1 minutt = 60 sekunder
+		int sekunder = secs % 60; //overskudd minutter
 		
-		// TODO 
+		String timestr = String.format("%02d%s%02d%s%02d", timer, TIMESEP, minutter, TIMESEP, sekunder);
 		
+		timestr = "  " + timestr;
+		
+		return timestr;
 	}
 	
 	private static int TEXTWIDTH = 10;
